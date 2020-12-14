cad895708f899f3acda558fcf5262c286e851f82
redamoon <yuuki60824@gmail.com>
2020-12-14 16:07:44 +0900

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,38 @@
 <template>
-  <div class="container mx-auto px-4">
-    <h2 class="text-lg font-bold">ブログ記事</h2>
-    <div v-for="(post, index) in posts" :key="index" class="grid grid-cols-3">
-      <div>
-        <a :href="`/blog/${post.id}/`">
-          {{ post.title }}
-        </a>
-      </div>
+  <div>
+    <h2 class="title is-3">記事の更新確認</h2>
+    <div v-for="(post, index) in posts" :key="index" class="panel">
+      <div class="panel-block">{{ post.title }}</div>
+    </div>
+    <h2 class="title is-3">ウェブページの更新確認</h2>
+    <div v-for="(post2, index) in pages" :key="index" class="panel">
+      <div class="panel-block">{{ post2.title }}</div>
+    </div>
+    <h2 class="title is-3">コンテンツタイプの更新確認</h2>
+    <div v-for="(post3, index) in content_data" :key="index" class="panel">
+      <div class="panel-block">{{ post3.label }}</div>
     </div>
   </div>
 </template>
 
 <script>
+// import axios from 'axios'
 export default {
-  async asyncData({ $axios, $config, error }) {
+  async asyncData({ $axios, error }) {
     try {
       const entries = await $axios.get(
-        `${$config.apiUrl}${$config.siteId}/entries`
+        'https://movabletype-knowledge.tech/cgi-bin/_mt_admin/mt-data-api.cgi/v4/sites/2/entries'
+      )
+      const pages = await $axios.get(
+        'https://movabletype-knowledge.tech/cgi-bin/_mt_admin/mt-data-api.cgi/v4/sites/2/pages'
+      )
+      const contentData = await $axios.get(
+        'https://movabletype-knowledge.tech/cgi-bin/_mt_admin/mt-data-api.cgi/v4/sites/2/contentTypes/3/data'
       )
       return {
         posts: entries.data.items,
+        pages: pages.data.items,
+        content_data: contentData.data.items,
       }
     } catch (err) {
       error({
@@ -29,5 +42,3 @@ export default {
   },
 }
 </script>
-
-<style></style>
