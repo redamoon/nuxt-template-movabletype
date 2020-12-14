e410754f60ce46aea39a0338dc8e75345b3654e2
redamoon <yuuki60824@gmail.com>
2020-12-14 15:39:22 +0900

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,33 @@
 <template>
   <div class="container mx-auto px-4">
     <h2 class="text-lg font-bold">ブログ記事</h2>
+    <div v-for="(post, index) in posts" :key="index" class="grid grid-cols-3">
+      <div>
+        <a :href="`/blog/${post.id}/`">
+          {{ post.title }}
+        </a>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  async asyncData({ $axios, $config, error }) {
+    try {
+      const entries = await $axios.get(
+        `${$config.apiUrl}${$config.siteId}/entries`
+      )
+      return {
+        posts: entries.data.items,
+      }
+    } catch (err) {
+      error({
+        errorCode: 404,
+      })
+    }
+  },
+}
+</script>
 
 <style></style>
